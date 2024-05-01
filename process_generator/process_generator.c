1057bb93f6ade239b6d52bf8076e1c9e9eda5e25
gumfreddy <55648002+gumfreddy@users.noreply.github.com>
2024-05-01 23:33:51 +0300

    gasdfawesfdaswfadsfds
    :)

--- process_generator/process_generator.c
+++ process_generator/process_generator.c
@@ -1,77 +1,139 @@
 #include "headers.h"
 #include "pri_queue.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <signal.h>
+#include <errno.h>
+key_t msgq_id;
+struct msgbuf
+{
+    long mtype;
+    process *p;
+};
+ // TODO: 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any. 
 void clearResources(int);
-int read_processes(pri_queue* processes);
 
-int main(int argc, char* argv[]) {
-	signal(SIGINT, clearResources);
-
-	// process queue (priority = arrivalTime) incase processes.txt isnt sorted by AT
-	pri_queue processesQueue;
-	pri_queue_init(&processesQueue);
-
-
-
-	pri_queue_free(&processesQueue);
-
-	// TODO Initialization
+int main(int argc, char *argv[])
+{
+    signal(SIGINT, clearResources);
 	// 1. Read the input files.
-	// 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-	// 3. Initiate and create the scheduler and clock processes.
-	// 4. Use this function after creating the clock process to initialize clock
-	initClk();
-	// To get time use this
-	int x = getClk();
-	printf("current time is %d\n", x);
-	// TODO Generation Main Loop
-	// 5. Create a data structure for processes and provide it with its parameters.
-	// 6. Send the information to the scheduler at the appropriate time.
-	// 7. Clear clock resources
-	destroyClk(true);
+    if (argc != 2)
+    {
+        return fprintf(stderr, "Incorrect usage, to run use: ./bin/process_generator.out <algorithm number>\n");
+    }
+    FILE *file = fopen("test.txt", "r");
+    if (file == NULL)
+    {
+        perror("Failed to open the file.\n");
+        return 1;
+    }
+    char *line;
+    size_t len = 0;
+    ssize_t read;
+
+    pri_queue *q = malloc(sizeof(*q));
+    pri_queue_init(q);
+
+    int id, remainingtime, arrivaltime, priority, numProcesses = 0;
+    process *p = malloc(sizeof(*p));
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        if (strncmp(line, "#", 1) != 0)
+        {
+            numProcesses++;
+            if (sscanf(line, "%d %d %d %d", &p->id, &p->arrivaltime, &p->remainingtime, &p->priority) != 4)
+            {
+                printf("Failed to parse line: %s\n", line);
+                continue;
+            }
+            pri_queue_enqueue(q, p->arrivaltime, p);
+            printf("Process with id %d, arrivaltime %d, remainingtime %d, priority %d\n", p->id, p->arrivaltime, p->remainingtime, p->priority);
+        }
+    }
+    print_pri_queue(q);
+    fclose(file);
+/////////////////////
+
+// 3. Initiate and create the scheduler and clock processes.
+    pid_t clk_child = fork();
+    if (clk_child == -1)
+    {
+        perror("Failed to fork clk_child");
+        exit(EXIT_FAILURE);
+    }
+
+    else if (clk_child == 0)
+    {
+        if (execl("./clk.out", "./clk.out", NULL) == -1)
+        {
+            perror("Failed to execl clk.out");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pid_t scheduler_child = fork();
+    if (scheduler_child == -1)
+    {
+        perror("Failed to fork scheduler_child");
+        exit(EXIT_FAILURE);
+    }
+
+    else if (scheduler_child == 0)
+    {
+        char *arr = malloc(sizeof(*arr));
+        sprintf(arr, "%d", numProcesses);
+        execl("./scheduler.out", "./scheduler.out", argv[1], arr, NULL);
+    }
+	/////////////////////////
+
+    initClk(); //initialize clock
+    int x = getClk();
+    printf("Current Time is %d\n", x);
+    // Message Queue Generation to send the process data to the scheduler
+    key_t msgq_id = msgget(MSGKEY, 0666 | IPC_CREAT);
+    if (msgq_id == -1)
+    {
+        perror("Error in create Message Queue");
+        exit(-1);
+    }
+    int send_val;
+    while (pri_queue_dequeue(q, (void **)&p))
+    {
+        printf("Process with id %d, arrivaltime %d, remainingtime %d, priority %d\n", p->id, p->arrivaltime, p->remainingtime, p->priority);
+        printf("%d\n", getClk());
+        if (p->arrivaltime > getClk())
+        {
+            printf("After Sleeping %d\n", p->arrivaltime - getClk());
+            sleep(p->arrivaltime - getClk());
+        }
+        print_pri_queue(q);
+        struct msgbuf *msg = malloc(sizeof(*msg));
+        msg->mtype = 1;
+        msg->p = p;
+        send_val = msgsnd(msgq_id, &msg, sizeof(msg->p), !IPC_NOWAIT);
+        if (send_val == -1)
+        {
+            perror("Error in send");
+            exit(-1);
+        }
+        printf("Process with id %d sent\n", p->id);
+    }
+    free(p);
+    pri_queue_free(q);
+    wait(NULL);
 }
 
-void clearResources(int signum) {
-	//TODO Clears all resources in case of interruption
+void clearResources(int signum)
+{
+    msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
+    destroyClk(true);
+    exit(0);
 }
-
-
-//int read_processes(pri_queue* processes) {
-//	if (!processes) return 0;
-//
-//	FILE* f = fopen("processses.txt", "r");
-//	if (!f) {
-//		// invalid file?
-//		return 0;
-//	}
-//	
-//	char* line = 0;
-//	size_t lineLen = 0;
-//	while (getline(&line, &lineLen, f) != EOF) {
-//		// we have a line :P
-//		// ignore empty lines or lines that start with #
-//		if (lineLen == 0 || 
-//			strlen(line) == 0 ||
-//			line[0] == '#') continue;
-//
-//		// allocate process
-//		struct process_data* p = malloc(sizeof(process_data));
-//		memset(p, 0, sizeof(process_data));
-//
-//		// read proc data
-//		sscanf(line, "%d%d%d%d", &p->id, &p->arrival_time, &p->running_time, &p->priority);
-//
-//		// insert in queue
-//		pri_queue_enqueue(processes, p->arrival_time, p);
-//	}
-//
-//	// close file
-//	fclose(f);
-//
-//	// free line
-//	if (line) {
-//		free(line);
-//	}
-//
-//	return 1;
-//}
--- shared/pri_queue.h
+++ shared/pri_queue.h
@@ -2,120 +2,150 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-typedef struct pri_queue_node {
-	struct pri_queue_node* next;
-	struct pri_queue_node* prev;
+typedef struct pri_queue_node
+{
+    struct pri_queue_node *next;
+    struct pri_queue_node *prev;
 
-	int priority;
-	void* value;
+    int priority;
+    void *value;
 } pri_queue_node;
 
 // priority queue
-typedef struct pri_queue {
-	struct pri_queue_node* head;
+typedef struct pri_queue
+{
+    struct pri_queue_node *head;
 } pri_queue;
 
-void pri_queue_init(pri_queue* q) {
-	if (!q) return;
+void pri_queue_init(pri_queue *q)
+{
+    if (!q)
+        return;
 
-	q->head = 0;
+    q->head = 0;
 }
 
-void pri_queue_free(pri_queue* q) {
-	if (!q) return;
+void pri_queue_free(pri_queue *q)
+{
+    if (!q)
+        return;
 
-	// free all nodes
-	struct pri_queue_node* n = q->head;
-	while (n) {
-		struct pri_queue_node* next = n->next;
+    // free all nodes
+    struct pri_queue_node *n = q->head;
+    while (n)
+    {
+        struct pri_queue_node *next = n->next;
 
-		// delete
-		free(n);
+        // delete
+        free(n);
 
-		n = next;
-	}
+        n = next;
+    }
 }
 
-void pri_queue_enqueue(pri_queue* q, int priority, void* value) {
-	if (!q) return;
+void pri_queue_enqueue(pri_queue *q, int priority, void *value)
+{
+    if (!q)
+        return;
 
-	// create node
-	struct pri_queue_node* node = (struct pri_queue_node*)malloc(sizeof(pri_queue_node));
-	memset(node, 0, sizeof(pri_queue_node));
+    // create node
+    struct pri_queue_node *node = (struct pri_queue_node *)malloc(sizeof(pri_queue_node));
+    memset(node, 0, sizeof(pri_queue_node));
 
-	node->priority = priority;
-	node->value = value;
+    node->priority = priority;
+    node->value = value;
 
-	// insert
-	if (!q->head) {
-		q->head = node;
-		return;
-	}
+    // insert
+    if (!q->head)
+    {
+        q->head = node;
+        return;
+    }
 
-	struct pri_queue_node* current = q->head;
-	while (current) {
-		if (current->priority > priority) {
-			// insert here
-			node->next = current;
-			node->prev = current->prev;
+    struct pri_queue_node *current = q->head;
+    while (current)
+    {
+        if (current->priority > priority)
+        {
+            // insert here
+            node->next = current;
+            node->prev = current->prev;
 
-			if (current->prev) {
-				current->prev->next = node;
-			}
-			current->prev = node;
+            if (current->prev)
+            {
+                current->prev->next = node;
+            }
+            current->prev = node;
 
-			if (current == q->head) {
-				q->head = node;
-			}
+            if (current == q->head)
+            {
+                q->head = node;
+            }
 
-			return;
-		}
+            return;
+        }
 
-		// not inserted yet, and next is null
-		if (!current->next) {
-			node->prev = current;
-			current->next = node;
+        // not inserted yet, and next is null
+        if (!current->next)
+        {
+            node->prev = current;
+            current->next = node;
 
-			return;
-		}
+            return;
+        }
 
-		current = current->next;
-	}
+        current = current->next;
+    }
 }
 
-int pri_queue_dequeue(pri_queue* q, void** value) {
-	if (!q || !q->head) return 0;
+int pri_queue_dequeue(pri_queue *q, void **value)
+{
+    if (!q || !q->head)
+        return 0;
 
-	struct pri_queue_node* n = q->head;
+    struct pri_queue_node *n = q->head;
 
-	// update head
-	q->head = n->next;
+    // update head
+    q->head = n->next;
 
-	// update prev
-	if (q->head) {
-		q->head->prev = 0;
-	}
+    // update prev
+    if (q->head)
+    {
+        q->head->prev = 0;
+    }
 
-	if (value) {
-		*value = n->value;
-	}
+    if (value)
+    {
+        *value = n->value;
+    }
 
-	// free node
-	free(n);
+    // free node
+    free(n);
 
-	return 1;
+    return 1;
 }
 
-//void print_pri_queue(pri_queue* q) {
-//	printf("Queue: [ ");
-//
-//	struct pri_queue_node* n = q->head;
-//	while (n) {
-//		printf("%d ", *(int*)n->value);
-//
-//		n = n->next;
-//	}
-//
-//	printf("]\n");
-//}
+void print_pri_queue(pri_queue *q)
+{
+    printf("Queue: [ ");
+    struct pri_queue_node *n = q->head;
+    while (n)
+    {
+        printf("%d ", n->priority);
+        //*(int *)n->value
+        n = n->next;
+    }
+
+    printf("]\n");
+}
+bool pri_queue_is_empty(pri_queue *q)
+{
+    return q->head == NULL;
+}
+
+void *pri_queue_peek(pri_queue *q)
+{
+    return q->head->value;
+}
